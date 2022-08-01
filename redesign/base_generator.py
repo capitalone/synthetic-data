2d4dc2ea5ecc8bd20f4e6bf96fd9b85400bf5852
jakleh <jslehner16@gmail.com>
2022-08-01 12:50:50 -0500

    Denoted abstract class and method in base_generator.py.

--- redesign/base_generator.py
+++ redesign/base_generator.py
@@ -1,11 +1,15 @@
 """Contains abstract class from which generators will inherit."""
-class BaseGenerator():
+
+import abc
+
+class BaseGenerator(metaclass=abc.ABCMeta):
     """Abstract generator class."""
 
     def __init__(self, profile):
         """Initialize generator object."""
         self.profile = profile.report()
 
+    @abc.abstractmethod
     def synthesize(self):
         """Make synthetic data."""
-        raise NotImplementedError("Not Implemented")
+        pass
