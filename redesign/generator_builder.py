0ed692420506458906c9e6f8d47ba782207853a0
jakleh <jslehner16@gmail.com>
2022-08-02 10:49:36 -0500

    Added decorator for class method and comments in generator_builder.py.

--- redesign/generator_builder.py
+++ redesign/generator_builder.py
@@ -5,14 +5,21 @@ from .generators import TabularGenerator, GraphGenerator, UnstructuredGenerator
 class Generator():
     """Generator-wrapper class."""
     
+    @classmethod
     def pick(profile):
         """Pick a generator."""
+
+        #Map string representations of generator classes to generator classes.
         types_dict = {
             "TabularGenerator": TabularGenerator,
             "GraphGenerator": GraphGenerator,
             "UnstructuredGenerator": UnstructuredGenerator
         }
+        
+        #Extract string representation from profile __str__() output.
         pick_type = str(profile).split(".")[3].split()[0]
+
+        #Return appropriate generator.
         return types_dict[pick_type]
 
     def __init__(self, profile):
