fix: precision and rmse

examples/eval_label_encoded_mse.py
@@ -170,11 +170,10 @@ model = RandomForestRegressor()
 # model = clf = MLPRegressor(solver='lbfgs', alpha=1e-5,
 #                             hidden_layer_sizes=(15,), random_state=1)
 clf = model.fit(X_train, y_train)
-original_mse = np.average((y_test - clf.predict(X_test))) ** 2
+original_mse = np.average((y_test - clf.predict(X_test)) ** 2)
 print(f"MSE of RandomForestRegressor on predicting price in original dataset: {original_mse}")
 
 X_synthetic = synthetic_data.drop(columns="price").astype(float)
 y_synthetic = synthetic_data['price'].astype(float)
-synthetic_mse = np.average((y_synthetic - clf.predict(X_synthetic))) ** 2
+synthetic_mse = np.average((y_synthetic - clf.predict(X_synthetic)) ** 2)
 print(f"MSE of RandomForestRegressor on predicting price in synthetic dataset: {synthetic_mse}")
-

examples/eval_one_hot_encoded_mse.py
@@ -161,12 +161,12 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1, random_
 model = clf = MLPRegressor(solver='lbfgs', alpha=1e-5,
                             hidden_layer_sizes=(15,), random_state=1)
 clf = model.fit(X_train, y_train)
-original_mse = np.average((y_test - clf.predict(X_test))) ** 2
+original_mse = np.average((y_test - clf.predict(X_test)) ** 2)
 print(f"MSE of RandomForestRegressor on predicting price in original dataset: {original_mse}")
 
 X_synthetic = synthetic_data.drop(columns="price").astype(float)
 y_synthetic = synthetic_data['price'].astype(float)
-synthetic_mse = np.average((y_synthetic - clf.predict(X_synthetic))) ** 2
+synthetic_mse = np.average((y_synthetic - clf.predict(X_synthetic)) ** 2)
 print(f"MSE of RandomForestRegressor on predicting price in synthetic dataset: {synthetic_mse}")
 
 print()
@@ -177,9 +177,9 @@ X_train, X_test, y_train, y_test = train_test_split(X_synthetic, y_synthetic, te
 model = clf = MLPRegressor(solver='lbfgs', alpha=1e-5,
                             hidden_layer_sizes=(15,), random_state=1)
 clf = model.fit(X_train, y_train)
-synthetic_mse = np.average((y_test - clf.predict(X_test))) ** 2
+synthetic_mse = np.average((y_test - clf.predict(X_test)) ** 2)
 print(f"MSE of RandomForestRegressor on predicting price in synthetic dataset: {synthetic_mse}")
 
 
-original_mse = np.average((y - clf.predict(X))) ** 2
+original_mse = np.average((y - clf.predict(X)) ** 2)
 print(f"MSE of RandomForestRegressor on predicting price in original dataset: {original_mse}")

synthetic_data/synthetic_data.py
@@ -458,26 +458,34 @@ def make_data_from_report(
         noise_level_x=noise_level,
         seed=seed,
         dist=dist,
+        scaler=None,
     )
 
-    # generate scalers by range of values in original data
-    scalers = {}
-    for col, stat in enumerate(report["data_stats"]):
-        _min = stat["statistics"]["min"]
-        _max = stat["statistics"]["max"]
-        scalers[col] = MinMaxScaler(feature_range=(_min, _max))
+    # # generate scalers by range of values in original data
+    # scalers = {}
+    # for col, stat in enumerate(report["data_stats"]):
+    #     _min = stat["statistics"]["min"]
+    #     _max = stat["statistics"]["max"]
+    #     scalers[col] = MinMaxScaler(feature_range=(_min, _max))
+    #
+    # # rescale to feature range
+    # for col in scalers:
+    #     x_final[:, col] = (
+    #         scalers[col].fit_transform(x_final[:, col].reshape(-1, 1)).flatten()
+    #     )
 
-    # rescale to feature range
-    for col in scalers:
-        x_final[:, col] = (
-            scalers[col].fit_transform(x_final[:, col].reshape(-1, 1)).flatten()
-        )
+    # # find number of decimals for each column and round the data to match
+    # precisions = [stat["samples"][0][::-1].find(".") for stat in report["data_stats"]]
 
-    # find number of decimals for each column and round the data to match
-    precisions = [stat["samples"][0][::-1].find(".") for stat in report["data_stats"]]
-
-    for i, precision in enumerate(precisions):
-        x_final[:, i] = np.around(x_final[:, i], precision if precision > 0 else 0)
+    for i, col_stat in enumerate(report["data_stats"]):
+        digits = 0
+        if col_stat['data_type'] not in ['int', 'float']:
+            continue
+        if col_stat['data_type'] in ['float']:
+            precision = col_stat.get('statistics', {}).get('precision', {}).get('max', 0)
+            digits = precision - np.ceil(np.log10(np.abs(x_final[:, i])))
+            digits = int((digits[np.isfinite(digits)]).max())
+        x_final[:, i] = np.around(x_final[:, i], digits)
 
     # replicate null values if null replication metrics exist in the original report
     col_to_null_metrics = {}