b4c570e7e939a28320880c9b19d4087ec7dedde7
Tony Wu <44908682+tonywu315@users.noreply.github.com>
2022-11-22 14:52:24 -0500

    Generator API (#274)
    * Redesign api
    
    * Space
    
    * Rename method and add test
    
    * Fix formatting and remove options
    
    * Rename
    
    * Undo
    
    * Update Generator API
    
    * Redesign and add tests
    
    * Remove parameter
    
    * Add test

--- redesign/generator_builder.py
+++ redesign/generator_builder.py
@@ -1,29 +1,47 @@
 """Contains generator factory class that picks generator based on its profile type."""
 
-from .generators import GraphGenerator, TabularGenerator, UnstructuredGenerator
 from dataprofiler import StructuredProfiler, UnstructuredProfiler
+
+from dataprofiler.profilers.graph_profiler import GraphProfiler
+
+from redesign.generators import GraphGenerator, TabularGenerator, UnstructuredGenerator
+
 
 class Generator:
     """Generator class."""
 
     valid_data_types = {
-        StructuredProfiler: TabularGenerator, 
-        UnstructuredProfiler: UnstructuredGenerator
+        StructuredProfiler: TabularGenerator,
+        UnstructuredProfiler: UnstructuredGenerator,
+        GraphProfiler: GraphGenerator,
     }
 
     @classmethod
     def is_valid_data(cls, profile):
         return profile.__class__ in cls.valid_data_types
 
-    def __new__(cls, profile, config=None, options=None):
+    def __new__(cls, seed=None, config=None, *args, **kwargs):
         """Instantiate proper generator."""
         if config:
             try:
-                return config(profile, options)
+                return config(*args, **kwargs)
             except Exception as e:
-                print("Warning: profile doesn't match user setting. \
-                        Proceeding with automatic generator selection...")         
-                        
-        if cls.is_valid_data(profile):
-            generator = cls.valid_data_types[profile.__class__]
-            return generator(profile, options)
+                raise ValueError("Warning: profile doesn't match user setting.")
+
+        profile = kwargs.pop("profile", None)
+        if not profile:
+            raise ValueError(
+                "No profile object was passed in kwargs. "
+                "If you want to generate synthetic data from a "
+                "profile, pass in a profile object through the "
+                'key "profile" in kwargs.'
+            )
+
+        if not cls.is_valid_data(profile):
+            raise ValueError(
+                "Profile object is invalid. The supported profile "
+                f"types are: {list(cls.valid_data_types.keys())}."
+            )
+
+        generator = cls.valid_data_types[profile.__class__]
+        return generator(profile, seed, *args, **kwargs)
--- redesign/generators.py
+++ redesign/generators.py
@@ -1,44 +1,55 @@
 """Contains generators for tabular, graph, and unstructured data profiles."""
 
-from .base_generator import BaseGenerator
+from synthetic_data.synthetic_data import make_data_from_report
+
+from redesign.base_generator import BaseGenerator
 
 
 class TabularGenerator(BaseGenerator):
     """Class for generating synthetic tabular data."""
 
-    def __init__(self, profile, options):
+    def __init__(self, profile, seed=None, noise_level: float = 0.0):
         """Initialize tabular generator object."""
-        super().__init__(profile, options)
+        super().__init__(profile, seed)
+        self.noise_level = noise_level
 
-    def synthesize(self):
+    def synthesize(self, num_samples: int, seed=None, noise_level: float = None):
         """
         Generate synthetic tabular data.
         """
-        if not self.options:
-            return "Synthesized tabular data!"
+        if seed is None:
+            seed = self.seed
+
+        if noise_level is None:
+            noise_level = self.noise_level
+
+        return make_data_from_report(
+            report=self.profile.report(),
+            n_samples=num_samples,
+            noise_level=noise_level,
+            seed=seed,
+        )
 
 
 class GraphGenerator(BaseGenerator):
     """Class for generating synthetic graph data."""
 
-    def __init__(self, profile, options):
+    def __init__(self, profile, seed):
         """Initialize graph generator object."""
-        super().__init__(profile, options)
+        super().__init__(profile, seed)
 
     def synthesize(self):
         """Generate synthetic graph data."""
-        if not self.options:
-            return "Synthesized graph data!"
+        raise NotImplementedError()
 
 
 class UnstructuredGenerator(BaseGenerator):
     """Class for generating synthetic tabular data."""
 
-    def __init__(self, profile, options):
+    def __init__(self, profile, seed):
         """Initialize unstructured generator object."""
-        super().__init__(profile, options)
+        super().__init__(profile, seed)
 
     def synthesize(self):
         """Generate synthetic unstructured data."""
-        if not self.options:
-            return "Synthesized unstructured data!"
+        raise NotImplementedError()
