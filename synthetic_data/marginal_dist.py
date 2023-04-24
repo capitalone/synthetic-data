2f39c6cf314842b31770ca84ec1d24110ece0667
tazitoo <snbbiz@gmail.com>
2023-04-24 18:19:24 -0400

    DataProfiler categoricals now default to multinomial (#284)
    * updated tests for categoricals
    
    * discrete distributions default to multinomial

--- synthetic_data/marginal_dist.py
+++ synthetic_data/marginal_dist.py
@@ -1,24 +1,21 @@
-from collections import Counter
-
-from scipy import stats
+"""
+Methods that support detecting and translating marginal distributions from a
+DataProfiler report into scipy.stats distributions used by synthetic_data
+"""
+import numpy as np
 
 
 def _detect_dist_continuous(col_stats):
     """
-    Detects type of continuous distribution based on Kolmogorov-Smirnov Goodness-of-fit test, https://en.wikipedia.org/wiki/Kolmogorov%E2%80%93Smirnov_test.
+    Detects type of continuous distribution based on Kolmogorov-Smirnov Goodness-of-fit test
+    https://en.wikipedia.org/wiki/Kolmogorov%E2%80%93Smirnov_test.
     Args:
-        col_stats (dict): Column data statistics. The column data must be of a continuous numerical random variable.
+        col_stats (dict): Column data statistics. The column data must be
+        a continuous numerical random variable.
     Returns:
-        dist (dict): Dictionary stating distribution type along with other parameters for the distribution.
+        dist (dict): Dictionary stating distribution type along with other parameters
+        for the distribution.
     """
-    bin_counts, bin_edges = (
-        col_stats["histogram"]["bin_counts"],
-        col_stats["histogram"]["bin_edges"],
-    )
-
-    # Create a continuous distribution from the histogram and sample data from it
-    hist_dist = stats.rv_histogram((bin_counts, bin_edges))
-    observed_samples = hist_dist.rvs(size=1000)
 
     # Distributions to test against (must be a continuous distribution from scipy.stats)
     # Distribution name -> list of positional arguments for the distribution
@@ -32,66 +29,45 @@ def _detect_dist_continuous(col_stats):
     )
 
     dist = {}
-    max_p = 0
-
-    for dist_name, dist_args in test_dists:
-
-        # overfitting on purpose for testing
-        # method = getattr(stats, dist_name)
-        # dist_args = method.fit(observed_samples)
-
-        p = stats.kstest(observed_samples, dist_name, dist_args)[1]
-        if p > max_p:
-            dist["dist"] = dist_name
-            dist["args"] = dist_args
-            max_p = p
+    dist["dist"] = "skewnorm"
+    dist["args"] = (col_stats["skewness"], col_stats["mean"], col_stats["stddev"])
+    dist["mean"] = col_stats["mean"]
+    dist["std"] = col_stats["stddev"]
 
     return dist
 
 
 def _detect_dist_discrete(col_stats):
     """
-    Detects type of discrete distribution based on Pearson's Chi-Squared Goodness-of-fit test, https://en.wikipedia.org/wiki/Chi-squared_test.
+    Constructs a scipy.stats.multinomial distribution for a feature based on
+    the columns stats provided by a DataProfiler report.
+
     Args:
-        col_stats (dict): Column data statistics. The column data must be of a discrete numerical random variable.
+        col_stats (dict): Column data statistics. The column data must be of a
+        discrete numerical random variable.
     Returns:
-        dist (dict): Dictionary stating distribution type along with other parameters for the distribution.
+        dist (dict): Dictionary stating distribution type along with other parameters
+        for the distribution.
     """
     # Convert strings to ints, ideally DataProfiler should be handling this
     categories = [int(category) for category in col_stats["categories"]]
-    categorical_count = {int(k): v for k, v in col_stats["categorical_count"].items()}
 
     categories.sort()
+    categorical_count = {int(k): v for k, v in col_stats["categorical_count"].items()}
+
     observed_freq = [categorical_count[category] for category in categories]
-    sample_size = col_stats["sample_size"]
+    p = np.array(observed_freq) / sum(observed_freq)
 
-    # Shift categories so they start at 0
-    categories = list(range(len(categories)))
-
-    # Distributions to test against (must be a discrete distribution from scipy.stats)
-    # Distribution name -> list of positional arguments for the distribution
-    # The min of the test distributions must be equal to min of categories, i.e. 0
-    test_dists = (
-        # binom(n, p)
-        *[("binom", (categories[-1], p * 0.25)) for p in range(1, 4)],
-        # randint(low, high)
-        ("randint", (0, categories[-1] + 1)),
-    )
+    assert p.sum() <= 1.00000000000005, f"ppf is too big, {p}"
+    assert p.sum() > 0.995, f"ppf is too small, {p}"
 
     dist = {}
-    max_p = 0
+    dist["dist"] = "multinomial"
+    dist["args"] = (1, p)
 
-    for dist_name, args in test_dists:
-        dist_method = getattr(stats, dist_name)
-        dist_method_specific = dist_method(*args)
-        test_samples = dist_method_specific.rvs(size=sample_size)
-        test_samples_count = Counter(test_samples)
-        expected_freq = [test_samples_count[category] for category in categories]
-        p = stats.chisquare(observed_freq, expected_freq)[1]
-        if p > max_p:
-            dist["dist"] = dist_name
-            dist["args"] = args
-            max_p = p
+    # categories need not be continuous
+    # think of this as labels to be applied to samples in multinomial_ppf()
+    dist["categories"] = categories
 
     return dist
 
@@ -108,9 +84,13 @@ def detect_dist(report):
         dist_list (list): List of dictionaries stating distribution type for each column
     """
     dist_list = []
+
     for col_num, col_dict in enumerate(report["data_stats"]):
         col_stats = col_dict["statistics"]
-        is_continuous = col_dict["data_type"] == "float"
+        is_continuous = col_dict["data_type"] == "float" or not col_dict["categorical"]
+        print(col_dict["column_name"], is_continuous)
+
+        print(col_num, col_dict["data_type"])
         dist = (
             _detect_dist_continuous(col_stats)
             if is_continuous
@@ -118,5 +98,7 @@ def detect_dist(report):
         )
         dist["column"] = col_num
         dist["column_name"] = col_dict["column_name"]
+        # print("marginal_dist line 121 ", dist)
         dist_list.append(dist)
+
     return dist_list
