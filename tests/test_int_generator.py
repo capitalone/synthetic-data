d35c1bc9173a7c0db33a0236c02f937bbe0f6a53
TCH323 <richard@bann.com>
2023-07-11 00:21:01 -0400

    random int generator and test

--- tests/test_int_generator.py
+++ tests/test_int_generator.py
@@ -1,18 +1,17 @@
-from synthetic_data.generators import TabularGenerator, UnstructuredGenerator, GraphGenerator
-from synthetic_data import Generator
-from synthetic_data.distinct_generators import int_generator
-import dataprofiler as dp
-import numpy as np
-import pandas as pd
 import os
 import unittest
 
+import dataprofiler as dp
+import numpy as np
+import pandas as pd
+import numpy as np
+
+from synthetic_data.distinct_generators.int_generator import IntGenerator
+
 test_dir = os.path.dirname(os.path.realpath(__file__))
 
-
-class TestRandomInts(unittest.TestCase):
-    @classmethod
-    def setUpClass(cls):
+class TestIntGenerator(unittest.TestCase):
+    def setUp(self):
         profile_options = dp.ProfilerOptions()
         profile_options.set(
             {
@@ -23,49 +22,26 @@ class TestRandomInts(unittest.TestCase):
         )
 
         # create dataset and profile for tabular
-        cls.tab_data = data = dp.Data(os.path.join(test_dir, "data/iris.csv"))
-        cls.tab_profile = dp.Profiler(
-            data, profiler_type="structured", options=profile_options
+        tab_data = dp.Data(os.path.join(test_dir, "data/iris.csv"))
+        tab_profile = dp.Profiler(
+            tab_data, profiler_type="structured", options=profile_options
         )
+        self.rng = np.random.default_rng(12345)        
+        self.generator = IntGenerator(tab_profile, self.rng)
+        self.np_array = self.generator.synthesize()
 
-        # create dataset and prfile for unstructured
-        data = pd.Series(["first string", "second string"])
-        cls.unstruct_profile = dp.Profiler(
-            data, profiler_type="unstructured", options=profile_options
-        )
+    def test_return_type(self):
+        self.assertIsInstance(self.np_array, np.ndarray)
+        for num in self.np_array:
+            self.assertIsInstance(num, np.int64)
 
-        # create dataset and prfile for graph datasets
-        data = dp.Data(os.path.join(test_dir, "data/graph.csv"))
-        cls.graph_profile = dp.Profiler(
-            data, profiler_type="graph", options=profile_options
-        )
+    def test_size(self):
+        self.assertEqual(self.np_array.shape[0], self.generator.num_rows)
 
-    def test_tabular_generator(self):
-        generator = Generator(profile=self.tab_profile, seed=42)
-        self.assertIsInstance(generator, TabularGenerator)
-        synthetic_data = generator.synthesize(100)
-        self.assertEqual(len(synthetic_data), 100)
-        random_seed = 0
-        rng = np.random.default_rng(seed=random_seed)
-        for col in synthetic_data:
-            col = int_generator.random_integers(rng)
-        
-        for col in synthetic_data:
-            self.assertIsInstance(type(col), int)
-
-    # def test_unstructured_generator(self):
-    #     generator = Generator(profile=self.tab_profile, seed=42)
-    #     self.assertIsInstance(generator, UnstructuredGenerator)
-    
-    # def test_graph_generator(self):
-    #     generator = Generator(profile=self.tab_profile, seed=42)
-    #     self.assertIsInstance(generator, GraphGenerator)
-    #     synthetic_data = generator.synthesize()
-    #     self.assertEqual(synthetic_data.number_of_nodes(), 278)
-    #     random_seed = 0
-    #     rng = np.random.default_rng(seed=random_seed)
-    #     for col in synthetic_data:
-    #         col = int_generator.random_integers(rng)
-        
-    #     for col in synthetic_data:
-    #         self.assertIsInstance(col, int)
+    def test_values_range(self):
+        min_value, max_value = self.generator.min, self.generator.max
+        for x in self.np_array:
+            print(x, min_value)
+            print(x, max_value)
+            self.assertTrue(x >= min_value)
+            self.assertTrue(x <= max_value)
