3efc788c930cd21b85dfeae309c25e61e448ff1f
lizlouise1335 <liz.smith@richmond.edu>
2023-07-11 11:08:00 -0400

    Datetime generator and tests

---
+++ synthetic_data/dataset_generators/datetime_generator.py
@@ -0,0 +1,84 @@
+import numpy as np
+import pandas as pd
+from numpy.random import Generator
+from typing import Optional
+
+
+def generate_datetime(
+    rng: Generator,
+    date_format: str,
+    start_date: Optional[str] = None,
+    end_date: Optional[str] = None,
+) -> str:
+    """
+    Generate datetime given the random_state, date_format, and start/end dates.
+
+    :param rng: the np rng object used to generate random values
+    :type rng: numpy Generator
+    :param date_format: the format that the generated datatime will follow,
+        defaults to None
+    :type date_format: str, None, optional
+    :param start_date: the earliest date that datetimes can be generated at,
+        defaults to None
+    :type start_date: pd.Timestamp, None, optional
+    :param start_date: the latest date that datetimes can be generated at,
+        defaults to None
+    :type start_date: pd.Timestamp, None, optional
+
+    :return: generated datetime
+    :rtype: str
+    """
+    if not start_date:
+        # 100 years in past
+        start_date = pd.Timestamp(1920, 1, 1)
+    if not end_date:
+        # protection of 30 years in future
+        end_date = pd.Timestamp(2049, 12, 31)
+    t = rng.random()
+    ptime = start_date + t * (end_date - start_date)
+
+    return ptime.strftime(date_format)
+
+
+def random_datetimes(
+    rng: Generator,
+    date_format_list: Optional[str] = None,
+    start_date: Optional[str] = None,
+    end_date: Optional[str] = None,
+    num_rows: int = 1,
+) -> np.array:
+    """
+    Generate datetime given the random_state, date_format, and start/end dates.
+
+    :param rng: the np rng object used to generate random values
+    :type rng: numpy Generator
+    :param date_format: the format that the generated datatime will follow,
+        defaults to None
+    :type date_format: str, None, optional
+    :param start_date: the earliest date that datetimes can be generated at,
+        defaults to None
+    :type start_date: pd.Timestamp, None, optional
+    :param start_date: the latest date that datetimes can be generated at,
+        defaults to None
+    :type start_date: pd.Timestamp, None, optional
+
+    :return: array of generated datetimes
+    :rtype: numpy array
+    """
+    date_list = []
+    if not date_format_list:
+        date_format_list = ["%B %d %Y %H:%M:%S"]
+
+    for _ in range(num_rows):
+        date_format = rng.choice(date_format_list)
+        datetime = generate_datetime(
+            rng, date_format=date_format, start_date=start_date, end_date=end_date
+        )
+        date_list.append(datetime)
+
+    return np.array(date_list)
+
+
+
+
+
---
+++ tests/test_datetime_generator.py
@@ -0,0 +1,54 @@
+
+
+import unittest
+from numpy.random import Generator, PCG64
+import pandas as pd
+import numpy as np
+import synthetic_data.dataset_generators.datetime_generator as dtg
+
+
+class TestDatetimeFunctions(unittest.TestCase):
+    def setUp(self):
+        self.rng = Generator(PCG64(12345))
+        self.start_date = pd.Timestamp(2001, 12, 22)
+        self.end_date = pd.Timestamp(2023, 1, 1)
+        self.date_format_list = ["%Y-%m-%d", "%d-%m-%Y"]
+
+    def test_generate_datetime(self):
+        date_str = dtg.generate_datetime(self.rng, 
+                                        self.date_format_list[0],
+                                        self.start_date,
+                                        self.end_date)
+        self.assertIsInstance(date_str, str)
+
+    def test_generate_datetime_format(self):
+        date_str = dtg.generate_datetime(self.rng, 
+                                        self.date_format_list[0],
+                                        self.start_date,
+                                        self.end_date)
+        format_check = True
+        try:
+            pd.to_datetime(date_str, format=self.date_format_list[0])
+        except ValueError:
+            format_check = False
+        self.assertTrue(format_check)
+
+    def test_generate_datetime_range(self):
+        date_str = dtg.generate_datetime(self.rng, 
+                                        self.date_format_list[0],
+                                        self.start_date,
+                                        self.end_date)
+        date_obj = pd.to_datetime(date_str, format = self.date_format_list[0])
+        self.assertTrue(self.start_date <= date_obj <= self.end_date)
+
+    # needs mock of generate datetime maybe
+    def test_random_datetimes_return_type_and_size(self):
+        result = dtg.random_datetimes(self.rng,
+                                      self.date_format_list,
+                                      self.start_date,
+                                      self.end_date,
+                                      5)
+        self.assertIsInstance(result, np.ndarray)
+        self.assertEqual(result.shape[0], 5)
+
+
