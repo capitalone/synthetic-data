40f62c09ab45a312c759410612a4c4f5fe1b6847
lizlouise1335 <liz.smith@richmond.edu>
2023-07-12 15:51:59 -0400

    categorical test + gen

---
+++ tests/test_categorical_generator.py
@@ -0,0 +1,24 @@
+import unittest
+import numpy as np
+from numpy.random import Generator, PCG64
+from synthetic_data.dataset_generators.categorical_generator import random_categorical
+
+
+class TestRandomsCategories(unittest.TestCase):
+    def setUp(self):
+        self.rng = Generator(PCG64(12345))
+        self.categories = ['X', 'Y', 'Z']
+
+    def test_default_return_validity(self):
+        result = random_categorical(self.rng, num_rows = 5)
+        self.assertIsInstance(result, np.ndarray)
+        self.assertTrue(set(result).issubset({"A", "B", "C", "D", "E"}))
+        self.assertEqual(result.shape[0], 5)
+    
+    def test_custom_return_validity(self):
+        result = random_categorical(self.rng, 
+                                    categories=self.categories, 
+                                    num_rows = 5)
+        self.assertIsInstance(result, np.ndarray)
+        self.assertTrue(set(result).issubset(self.categories))
+        self.assertEqual(result.shape[0], 5)        
