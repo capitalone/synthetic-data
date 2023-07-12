cc19e22b88f160f5530a99158e35c22bb6afaea3
TCH323 <richard@bann.com>
2023-07-12 17:38:25 -0400

    passed pre-commit checks for black hook

--- tests/distinct_generators/test_text_generator.py
+++ tests/distinct_generators/test_text_generator.py
@@ -22,14 +22,14 @@ class TestTextGeneratorFunctions(unittest.TestCase):
         txt_arr = random_text(self.rng, str_len_min=256, str_len_max=1000)
         with self.assertRaises(ValueError):
             random_text(self.rng, str_len_min=255)
-        
+
         self.assertLessEqual(len(str_arr[0]), 256)
         self.assertGreaterEqual(len(str_arr[0]), 1)
         self.assertLessEqual(len(txt_arr[0]), 1000)
         self.assertGreaterEqual(len(txt_arr[0]), 256)
 
     def test_num_rows(self):
-        num_rows = [1,5,10]
+        num_rows = [1, 5, 10]
         for nr in num_rows:
             str_arr = random_string(self.rng, num_rows=nr)
             txt_arr = random_text(self.rng, num_rows=nr)
@@ -37,9 +37,9 @@ class TestTextGeneratorFunctions(unittest.TestCase):
             self.assertEqual(txt_arr.size, nr)
 
     def test_chars(self):
-        chars_set = {"0","1"}
-        str_arr = random_string(self.rng, chars=["0","1"])
-        txt_arr = random_text(self.rng, chars=["0","1"])
+        chars_set = {"0", "1"}
+        str_arr = random_string(self.rng, chars=["0", "1"])
+        txt_arr = random_text(self.rng, chars=["0", "1"])
         for s in str_arr:
             for char in s:
                 self.assertIn(char, chars_set)
