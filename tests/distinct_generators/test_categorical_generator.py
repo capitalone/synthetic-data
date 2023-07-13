909bcad6355e728af99bbfd807757ee814336463
lizlouise1335 <liz.smith@richmond.edu>
2023-07-13 10:36:27 -0400

    black

--- tests/distinct_generators/test_categorical_generator.py
+++ tests/distinct_generators/test_categorical_generator.py
@@ -9,23 +9,21 @@ from synthetic_data.distinct_generators.categorical_generator import random_cate
 class TestRandomsCategories(unittest.TestCase):
     def setUp(self):
         self.rng = Generator(PCG64(12345))
-        self.categories = ['People', 'Cats', 'Dogs']
+        self.categories = ["People", "Cats", "Dogs"]
 
     def test_default_return_validity(self):
-        result = random_categorical(self.rng, num_rows = 5)
+        result = random_categorical(self.rng, num_rows=5)
         self.assertIsInstance(result, np.ndarray)
         self.assertTrue(set(result).issubset({"A", "B", "C", "D", "E"}))
         self.assertEqual(result.shape[0], 5)
 
-        result = random_categorical(self.rng, num_rows = 4)
+        result = random_categorical(self.rng, num_rows=4)
         self.assertIsInstance(result, np.ndarray)
         self.assertTrue(set(result).issubset({"A", "B", "C", "D", "E"}))
         self.assertEqual(result.shape[0], 4)
-    
+
     def test_custom_return_validity(self):
-        result = random_categorical(self.rng, 
-                                    categories=self.categories, 
-                                    num_rows = 2)
+        result = random_categorical(self.rng, categories=self.categories, num_rows=2)
         self.assertIsInstance(result, np.ndarray)
         self.assertTrue(set(result).issubset(self.categories))
-        self.assertEqual(result.shape[0], 2)       
+        self.assertEqual(result.shape[0], 2)
