e46df641e7b8c47a76b0f693a50bc87f336930d3
lizlouise1335 <liz.smith@richmond.edu>
2023-07-25 16:06:42 -0400

    WIP tests

--- tests/test_dataset_generator.py
+++ tests/test_dataset_generator.py
@@ -35,63 +35,63 @@ class TestDatasetGenerator(unittest.TestCase):
 
     def test_generate_custom_datasets(self):
         columns_to_gen = [
-            {"generator": "integer", "name": "Name1", "min_value": 0, "max_value": 1},
+            {"generator": "integer", "name": "int", "min_value": 4, "max_value": 88},
             {
                 "generator": "datetime",
-                "name": "Name2",
-                "start_date": pd.Timestamp(2049, 12, 31),
+                "name": "dat",
+                "start_date": pd.Timestamp(2001, 12, 22),
+                "end_date": pd.Timestamp(2022, 12, 22),
             },
             {
                 "generator": "text",
-                "name": "Name3",
+                "name": "txt",
                 "chars": ["a", "b", "c"],
                 "str_len_min": 300,
                 "str_len_max": 301,
             },
-        ]
-        expected_names = ["Name1", "Name2", "Name3"]
-        df = generate_dataset_by_class(
-            self.rng,
-            columns_to_generate=columns_to_gen,
-            dataset_length=self.dataset_length,
-            path=None,
-        )
-        self.assertEqual(
-            self.dataset_length, len(df)
-        )  # remove once exact assert implemented
-        self.assertListEqual(list(df.columns), expected_names)
-
-        columns_to_gen = [
             {
                 "generator": "string",
-                "name": "Name4",
+                "name": "str",
                 "chars": ["a", "b", "c"],
                 "str_len_min": 2,
                 "str_len_max": 5,
             },
             {
                 "generator": "categorical",
-                "name": "Name5",
+                "name": "cat",
                 "categories": ["X", "Y", "Z"],
                 "probabilities": [0.1, 0.5, 0.4],
             },
             {
                 "generator": "float",
-                "name": "Name6",
+                "name": "flo",
                 "min_value": 3,
                 "max_value": 10,
                 "sig_figs": 3,
             },
         ]
-        expected_names = ["Name4", "Name5", "Name6"]
+        expected_names = ["int", "dat", "txt", "str", "cat", "flo"]
         df = generate_dataset_by_class(
             self.rng,
             columns_to_generate=columns_to_gen,
-            dataset_length=4,
+            dataset_length=self.dataset_length,
             path=None,
         )
-        self.assertEqual(4, len(df))  # remove once exact assert implemented
         self.assertListEqual(list(df.columns), expected_names)
+        assert (
+            df["int"].between(4, 88).all()
+        ), "Some integer values in 'int' are not in the specified range."
+        assert (
+            df["dat"]
+            .between(pd.Timestamp(2001, 12, 22), pd.Timestamp(2022, 12, 22))
+            .all()
+        ), "Some dates in 'dat' are not in the specified range."
+        # assert df["txt"].apply(pd.Timestamp(2001, 12, 22), pd.Timestamp(2022, 12, 22)).all(), "Some dates in 'dat' are not in the specified range."
+        # string
+        # categorical
+        assert (
+            df["flo"].between(4, 88).all()
+        ), "Some floats in 'flo' are not in the specified range."
 
     @mock.patch("synthetic_data.dataset_generator.pd.DataFrame.to_csv")
     def test_path_to_csv(self, to_csv):
