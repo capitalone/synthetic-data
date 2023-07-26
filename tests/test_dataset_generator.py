1d988f8fa5cb7a6d2f9c6a335a4c83d3a65767db
lizlouise1335 <liz.smith@richmond.edu>
2023-07-26 13:12:37 -0400

    last commit? plz?

--- tests/test_dataset_generator.py
+++ tests/test_dataset_generator.py
@@ -11,30 +11,7 @@ class TestDatasetGenerator(unittest.TestCase):
     def setUp(self):
         self.rng = Generator(PCG64(12345))
         self.dataset_length = 10
-
-    def test_generate_dataset_with_invalid_generator(self):
-        columns_to_gen = [{"generator": "non existent generator"}]
-        with self.assertRaisesRegex(
-            ValueError, "generator: non existent generator is not a valid generator."
-        ):
-            generate_dataset_by_class(
-                self.rng,
-                columns_to_generate=columns_to_gen,
-                dataset_length=self.dataset_length,
-                path=None,
-            )
-
-    @mock.patch("synthetic_data.dataset_generator.logging.warning")
-    def test_generate_dataset_with_none_columns(self, mock_warning):
-        empty_dataframe = pd.DataFrame()
-        df = generate_dataset_by_class(self.rng, None, self.dataset_length, None)
-        mock_warning.assert_called_once_with(
-            "columns_to_generate is empty, empty dataframe will be returned."
-        )
-        self.assertEqual(empty_dataframe.empty, df.empty)
-
-    def test_generate_custom_dataset(self):
-        columns_to_gen = [
+        self.columns_to_gen = [
             {"generator": "integer", "name": "int", "min_value": 4, "max_value": 88},
             {
                 "generator": "datetime",
@@ -70,10 +47,33 @@ class TestDatasetGenerator(unittest.TestCase):
                 "sig_figs": 3,
             },
         ]
+
+    def test_generate_dataset_with_invalid_generator(self):
+        columns_to_gen = [{"generator": "non existent generator"}]
+        with self.assertRaisesRegex(
+            ValueError, "generator: non existent generator is not a valid generator."
+        ):
+            generate_dataset_by_class(
+                self.rng,
+                columns_to_generate=columns_to_gen,
+                dataset_length=self.dataset_length,
+                path=None,
+            )
+
+    @mock.patch("synthetic_data.dataset_generator.logging.warning")
+    def test_generate_dataset_with_none_columns(self, mock_warning):
+        empty_dataframe = pd.DataFrame()
+        df = generate_dataset_by_class(self.rng, None, self.dataset_length, None)
+        mock_warning.assert_called_once_with(
+            "columns_to_generate is empty, empty dataframe will be returned."
+        )
+        self.assertEqual(empty_dataframe.empty, df.empty)
+
+    def test_generate_custom_dataset(self):
         expected_names = ["int", "dat", "txt", "str", "cat", "flo"]
         df = generate_dataset_by_class(
             self.rng,
-            columns_to_generate=columns_to_gen,
+            columns_to_generate=self.columns_to_gen,
             dataset_length=self.dataset_length,
             path=None,
         )
@@ -110,16 +110,11 @@ class TestDatasetGenerator(unittest.TestCase):
         :param to_csv: mock of Pandas to_csv()
         :type to_csv: func
         """
-        columns_to_gen = [
-            {"generator": "integer"},
-            {"generator": "datetime"},
-            {"generator": "text"},
-        ]
         to_csv.return_value = "assume Pandas to_csv for a dataframe runs correctly"
         path = "testing_path"
         generate_dataset_by_class(
             self.rng,
-            columns_to_generate=columns_to_gen,
+            columns_to_generate=self.columns_to_gen,
             dataset_length=4,
             path=path,
         )
