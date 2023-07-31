2b2459eb49cbadff4c03c8221f7acbe04dd3e482
drahc1R <richard@bann.com>
2023-07-31 11:38:50 -0400

    connected tabular generator with dataset_generator

--- synthetic_data/generators.py
+++ synthetic_data/generators.py
@@ -1,9 +1,11 @@
 """Contains generators for tabular, graph, and unstructured data profiles."""
 
 import dataprofiler as dp
+import numpy as np
 from sklearn import preprocessing
 
 from synthetic_data.base_generator import BaseGenerator
+from synthetic_data.dataset_generator import generate_dataset
 from synthetic_data.graph_synthetic_data import GraphDataGenerator
 from synthetic_data.synthetic_data import make_data_from_report
 
@@ -11,11 +13,13 @@ from synthetic_data.synthetic_data import make_data_from_report
 class TabularGenerator(BaseGenerator):
     """Class for generating synthetic tabular data."""
 
-    def __init__(self, profile, seed=None, noise_level: float = 0.0, is_correlated: bool = True):
+    def __init__(
+        self, profile, seed=None, noise_level: float = 0.0, method: str = "correlated"
+    ):
         """Initialize tabular generator object."""
         super().__init__(profile, seed)
         self.noise_level = noise_level
-        self.is_correlated = is_correlated
+        self.method = method
 
     @classmethod
     def post_profile_processing_w_data(cls, data, profile):
@@ -48,7 +52,9 @@ class TabularGenerator(BaseGenerator):
             )
         return profile
 
-    def synthesize(self, num_samples: int, seed=None, noise_level: float = None):
+    def synthesize(
+        self, num_samples: int, seed=None, noise_level: float = None, **kwargs
+    ):
         """Generate synthetic tabular data."""
         if seed is None:
             seed = self.seed
@@ -56,13 +62,39 @@ class TabularGenerator(BaseGenerator):
         if noise_level is None:
             noise_level = self.noise_level
 
-        return make_data_from_report(
-            report=self.profile.report(),
-            n_samples=num_samples,
-            noise_level=noise_level,
-            is_correlated=self.is_correlated,
-            seed=seed,
-        )
+        if self.method == "correlated":
+            return make_data_from_report(
+                report=self.profile.report(),
+                n_samples=num_samples,
+                noise_level=noise_level,
+                seed=seed,
+            )
+        else:
+
+            random_seed = 0
+            rng = np.random.default_rng(seed=random_seed)
+            columns = self.profile.report()["data_stats"]
+            col_data = []
+
+            for i, col in enumerate(columns):
+                data_type = col[i].get("data_type", None)
+                ordered = col[i].get("order", None)
+
+                if data_type == "datetime" and "date_format_list" in kwargs:
+                    col_data.append(
+                        {
+                            "data_type": data_type,
+                            "ordered": ordered,
+                            "date_format_list": kwargs["date_format_list"],
+                        }
+                    )
+                else:
+                    col_data.append({"data_type": data_type, "ordered": ordered})
+
+            return generate_dataset(
+                rng=rng,
+                columns_to_generate=col_data,
+            )
 
 
 class UnstructuredGenerator(BaseGenerator):
