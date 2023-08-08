194df502646472c1ad234516b63456fdf6ea2f09
drahc1R <richard@bann.com>
2023-08-08 11:00:46 -0400

    added fixes from prev reverted commit

--- tests/test_generators.py
+++ tests/test_generators.py
@@ -65,193 +65,54 @@ class TestTabularGenerator(unittest.TestCase):
         correlated_tabular_generator.synthesize(num_samples=10)
         mock_make_data.assert_called_once()
 
-    # rough draft
-    # @mock.patch(
-    #     "synthetic_data.generators.TabularGenerator.generate_uncorrelated_column_data"
-    # )
-    # def test_uncorrelated_synthesize_columns_to_generate(
-    #     self, mock_generate_uncorrelated_cols
-    # ):
 
-    #     generator = TabularGenerator(profile=self.profile, is_correlated=False, seed=42)
-    #     self.assertFalse(generator.is_correlated)
-    #     expected_columns_to_generate = [
-    #         {
-    #             "generator": "datetime",
-    #             "name": "dat",
-    #             "date_format_list": ["%m/%d/%y %H:%M"],
-    #             "start_date": pd.Timestamp("2013-03-03 21:53:00"),
-    #             "end_date": pd.Timestamp("2013-03-03 22:21:00"),
-    #             "order": "random",
-    #         },
-    #         {
-    #             "generator": "categorical",
-    #             "name": "cat",
-    #             "categories": [
-    #                 "groucho-oregon",
-    #                 "groucho-singapore",
-    #                 "groucho-tokyo",
-    #                 "groucho-sa",
-    #                 "zeppo-norcal",
-    #                 "groucho-norcal",
-    #                 "groucho-us-east",
-    #                 "groucho-eu",
-    #             ],
-    #             "probabilities": [
-    #                 0.3333333333333333,
-    #                 0.2,
-    #                 0.13333333333333333,
-    #                 0.06666666666666667,
-    #                 0.06666666666666667,
-    #                 0.06666666666666667,
-    #                 0.06666666666666667,
-    #                 0.06666666666666667,
-    #             ],
-    #             "order": "random",
-    #         },
-    #         {
-    #             "generator": "integer",
-    #             "name": "int",
-    #             "min_value": 1007884304.0,
-    #             "max_value": 3730416887.0,
-    #             "order": "random",
-    #         },
-    #         {
-    #             "generator": "categorical",
-    #             "name": "cat",
-    #             "categories": ["TCP", "UDP"],
-    #             "probabilities": [0.8823529411764706, 0.11764705882352941],
-    #             "order": "random",
-    #         },
-    #         {
-    #             "generator": "integer",
-    #             "name": "int",
-    #             "min_value": 2489.0,
-    #             "max_value": 56577.0,
-    #             "order": "random",
-    #         },
-    #         {
-    #             "generator": "integer",
-    #             "name": "int",
-    #             "min_value": 22.0,
-    #             "max_value": 5060.0,
-    #             "order": "random",
-    #         },
-    #         {
-    #             "generator": "text",
-    #             "name": "txt",
-    #             "chars": ["4", "3", "0", "7", ".", "5", "1", "8", "2", "6", "9"],
-    #             "str_len_min": 11.0,
-    #             "str_len_max": 15.0,
-    #             "order": "random",
-    #         },
-    #         {
-    #             "generator": "categorical",
-    #             "name": "cat",
-    #             "categories": [
-    #                 "Seoul",
-    #                 "Jiangxi Sheng",
-    #                 "Taipei",
-    #                 "Oregon",
-    #                 "Illinois",
-    #                 "Henan Sheng",
-    #                 "Sichuan Sheng",
-    #                 "Hebei",
-    #                 "Liaoning",
-    #                 "Washington",
-    #             ],
-    #             "probabilities": [
-    #                 0.3333333333333333,
-    #                 0.13333333333333333,
-    #                 0.06666666666666667,
-    #                 0.06666666666666667,
-    #                 0.06666666666666667,
-    #                 0.06666666666666667,
-    #                 0.06666666666666667,
-    #                 0.06666666666666667,
-    #                 0.06666666666666667,
-    #                 0.06666666666666667,
-    #             ],
-    #             "order": "random",
-    #         },
-    #         {
-    #             "generator": "categorical",
-    #             "name": "cat",
-    #             "categories": ["11", "36", "OR", "IL", "41", "51", "13", "21", "WA"],
-    #             "probabilities": [
-    #                 0.35714285714285715,
-    #                 0.14285714285714285,
-    #                 0.07142857142857142,
-    #                 0.07142857142857142,
-    #                 0.07142857142857142,
-    #                 0.07142857142857142,
-    #                 0.07142857142857142,
-    #                 0.07142857142857142,
-    #                 0.07142857142857142,
-    #             ],
-    #             "order": "random",
-    #         },
-    #         {
-    #             "generator": "integer",
-    #             "name": "int",
-    #             "min_value": 60661.0,
-    #             "max_value": 98168.0,
-    #             "order": "descending",
-    #         },
-    #         {
-    #             "generator": "float",
-    #             "name": "flo",
-    #             "min_value": 25.0392,
-    #             "max_value": 51.0,
-    #             "sig_figs": 6,
-    #             "order": "random",
-    #         },
-    #         {
-    #             "generator": "float",
-    #             "name": "flo",
-    #             "min_value": -122.9117,
-    #             "max_value": 127.02,
-    #             "sig_figs": 7,
-    #             "order": "random",
-    #         },
-    #         {
-    #             "generator": "integer",
-    #             "name": "int",
-    #             "min_value": 664.0,
-    #             "max_value": 9464.0,
-    #             "order": "random",
-    #         },
-    #     ]
-    #     generator.synthesize(20)
+# @mock.patch("generate_uncorrelated_column_data.TabularGenerator", spec=TabularGenerator)
+class TestDatasetGenerator(unittest.TestCase):
+    # @staticmethod
+    # def setup_tabular_generator_mock(mock_generator):
+    # mock_DataLabeler = mock_generator.return_value
 
-    #     # checking a list of dictionaries.
-    #     # this will check if generated params is correct
+    def setUp(self):
+        self.dataset_length = 10
+        self.rng = np.random.Generator(np.random.PCG64(12345))
+        self.columns_to_gen = [
+            {"generator": "integer", "name": "int", "min_value": 4, "max_value": 88},
+            {
+                "generator": "datetime",
+                "name": "dat",
+                "date_format_list": ["%Y-%m-%d"],
+                "start_date": pd.Timestamp(2001, 12, 22),
+                "end_date": pd.Timestamp(2022, 12, 22),
+            },
+            {
+                "generator": "text",
+                "name": "txt",
+                "chars": ["0", "1"],
+                "str_len_min": 2,
+                "str_len_max": 5,
+            },
+            {
+                "generator": "categorical",
+                "name": "cat",
+                "categories": ["X", "Y", "Z"],
+                "probabilities": [0.1, 0.5, 0.4],
+            },
+            {
+                "generator": "float",
+                "name": "flo",
+                "min_value": 3,
+                "max_value": 10,
+                "sig_figs": 3,
+            },
+        ]
 
-    #     # mock_generate_uncorrelated_cols is a func that returns
-    #     # a dataframe of synthetic data
-    #     # need to check if convert_data_to_df -> check for args
-    #     mock_generate_uncorrelated_cols.assert_called_once()
-
-    #     # we want to check if convert_data_to_df is called with the correct params
-    #     for i in range(len(expected_columns_to_generate)):
-    #         for key in mock_generate_uncorrelated_cols[i].keys():
-    #             if isinstance(mock_generate_uncorrelated_cols[i][key], list):
-    #                 self.assertTrue(
-    #                     set(mock_generate_uncorrelated_cols[i][key]).issubset(
-    #                         expected_columns_to_generate[i][key]
-    #                     )
-    #                 )
-    #             else:
-    #                 self.assertEqual(
-    #                     mock_generate_uncorrelated_cols[i][key], expected_columns_to_generate[i][key]
-    #                 )
     @mock.patch("synthetic_data.generators.random_integers")
     @mock.patch("synthetic_data.generators.random_floats")
     @mock.patch("synthetic_data.generators.random_categorical")
     @mock.patch("synthetic_data.generators.random_datetimes")
     @mock.patch("synthetic_data.generators.random_text")
     @mock.patch(
-        "synthetic_data.generators.TabularGenerator.generate_uncorrelated_column_data"
+        "synthetic_data.generators.TabularGenerator._generate_uncorrelated_column_data"
     )
     def test_generate_uncorrelated_column_data(
         self,
@@ -362,7 +223,7 @@ class TestTabularGenerator(unittest.TestCase):
                 mock.call(rng=generator.rng, min=22.0, max=5060.0, num_rows=20),
                 mock.call(rng=generator.rng, min=60661.0, max=98168.0, num_rows=20),
             ],
-            # text
+            # text calls
             [
                 mock.call(
                     rng=generator.rng,
@@ -372,7 +233,7 @@ class TestTabularGenerator(unittest.TestCase):
                     num_rows=20,
                 )
             ],
-            # float
+            # float calls
             [
                 mock.call(
                     rng=generator.rng,
@@ -392,9 +253,8 @@ class TestTabularGenerator(unittest.TestCase):
             ],
         ]
 
-        actual_df = generator.synthesize(20)
+        generator.synthesize(20)
 
-        # check to see if columns were generated
         mock_generate_uncorrelated_cols.assert_called_once()
 
         random_generators = [
@@ -405,33 +265,10 @@ class TestTabularGenerator(unittest.TestCase):
             mock_random_integers,
         ]
 
-        # check if each generator was called with the correct params
-        # iterate through the mocked random generators
         for i, generator in enumerate(random_generators):
-            # get the args_list for each generator
             call_args_list = generator.call_args_list
-            # expected_calls[i] is a mock.call(expected_params),
-            # and this corresponds to generator for each generator in generators.
-            # this wont work since list params can differ
-            # self.assertEqual(call_args_list, expected_calls[i])
-
-            # possible solution to randomized list parameters:
-            # call_args_list returns (*args, **kwargs)
-            # it's prob always kwargs so:
-            # call_args_list[0] -> {param_name:var}
-            # iterate through this dictionary of params
-            # and basically check if the value is a list, if it is,
-            # assertCountEqual for that
             for j, key in enumerate(call_args_list):
-                # if the current value is a list, compare that with the expected
-                # expected is a mock call -> ({param:value})
                 if isinstance(call_args_list[key], list):
-                    # get ith expected call list. format:
-                    # expected_calls = [
-                    #     [
-                    #         {"categories": ["blue", "yellow"]}
-                    #     ]
-                    # ]
                     self.assertCountEqual(
                         call_args_list[key], expected_calls[i][0][key]
                     )
@@ -439,121 +276,81 @@ class TestTabularGenerator(unittest.TestCase):
                 else:
                     self.assertEqual(call_args_list[key], expected_calls[j][key])
 
+    # def test_get_ordered_column_integration(self):
+    #     columns_to_gen = [
+    #         {
+    #             "generator": "integer",
+    #             "name": "int",
+    #             "min_value": 4,
+    #             "max_value": 88,
+    #             "order": "ascending",
+    #         },
+    #         {
+    #             "generator": "datetime",
+    #             "name": "dat",
+    #             "date_format_list": ["%Y-%m-%d"],
+    #             "start_date": pd.Timestamp(2001, 12, 22),
+    #             "end_date": pd.Timestamp(2022, 12, 22),
+    #             "order": "ascending",
+    #         },
+    #         {
+    #             "generator": "text",
+    #             "name": "txt",
+    #             "chars": ["0", "1"],
+    #             "str_len_min": 2,
+    #             "str_len_max": 5,
+    #             "order": "ascending",
+    #         },
+    #         {
+    #             "generator": "categorical",
+    #             "name": "cat",
+    #             "categories": ["X", "Y", "Z"],
+    #             "probabilities": [0.1, 0.5, 0.4],
+    #             "order": "ascending",
+    #         },
+    #         {
+    #             "generator": "float",
+    #             "name": "flo",
+    #             "min_value": 3,
+    #             "max_value": 10,
+    #             "sig_figs": 3,
+    #             "order": "ascending",
+    #         },
+    #     ]
+    #     expected_data = [
+    #         np.array([21, 23, 30, 36, 57, 60, 62, 70, 70, 87]),
+    #         np.array(
+    #             [
+    #                 "2003-12-27",
+    #                 "2005-11-23",
+    #                 "2007-03-10",
+    #                 "2008-12-17",
+    #                 "2011-04-02",
+    #                 "2014-07-16",
+    #                 "2015-12-26",
+    #                 "2016-02-07",
+    #                 "2021-10-01",
+    #                 "2021-11-24",
+    #             ]
+    #         ),
+    #         np.array(
+    #             ["00", "000", "0001", "01", "0100", "10", "10", "100", "1110", "1111"]
+    #         ),
+    #         np.array(["Y", "Y", "Y", "Y", "Y", "Y", "Z", "Z", "Z", "Z"]),
+    #         np.array(
+    #             [3.035, 3.477, 4.234, 4.812, 4.977, 5.131, 5.379, 5.488, 7.318, 7.4]
+    #         ),
+    #     ]
+    #     expected_df = pd.DataFrame.from_dict(
+    #         dict(zip(["int", "dat", "txt", "cat", "flo"], expected_data))
+    #     )
+    #     actual_df = dataset_generator.generate_dataset(
+    #         self.rng,
+    #         columns_to_generate=columns_to_gen,
+    #         dataset_length=self.dataset_length,
+    #     )
+    #     np.testing.assert_array_equal(actual_df.values, expected_df.values)
 
-# # @mock.patch("generate_uncorrelated_column_data.TabularGenerator", spec=TabularGenerator)
-# class TestDatasetGenerator(unittest.TestCase):
-#     # @staticmethod
-#     # def setup_tabular_generator_mock(mock_generator):
-#         # mock_DataLabeler = mock_generator.return_value
-
-#     def setUp(self):
-#         self.dataset_length = 10
-#         self.rng = np.random.Generator(np.random.PCG64(12345))
-#         self.columns_to_gen = [
-#             {"generator": "integer", "name": "int", "min_value": 4, "max_value": 88},
-#             {
-#                 "generator": "datetime",
-#                 "name": "dat",
-#                 "date_format_list": ["%Y-%m-%d"],
-#                 "start_date": pd.Timestamp(2001, 12, 22),
-#                 "end_date": pd.Timestamp(2022, 12, 22),
-#             },
-#             {
-#                 "generator": "text",
-#                 "name": "txt",
-#                 "chars": ["0", "1"],
-#                 "str_len_min": 2,
-#                 "str_len_max": 5,
-#             },
-#             {
-#                 "generator": "categorical",
-#                 "name": "cat",
-#                 "categories": ["X", "Y", "Z"],
-#                 "probabilities": [0.1, 0.5, 0.4],
-#             },
-#             {
-#                 "generator": "float",
-#                 "name": "flo",
-#                 "min_value": 3,
-#                 "max_value": 10,
-#                 "sig_figs": 3,
-#             },
-#         ]
-
-#     def test_get_ordered_column_integration(self):
-#         columns_to_gen = [
-#             {
-#                 "generator": "integer",
-#                 "name": "int",
-#                 "min_value": 4,
-#                 "max_value": 88,
-#                 "order": "ascending",
-#             },
-#             {
-#                 "generator": "datetime",
-#                 "name": "dat",
-#                 "date_format_list": ["%Y-%m-%d"],
-#                 "start_date": pd.Timestamp(2001, 12, 22),
-#                 "end_date": pd.Timestamp(2022, 12, 22),
-#                 "order": "ascending",
-#             },
-#             {
-#                 "generator": "text",
-#                 "name": "txt",
-#                 "chars": ["0", "1"],
-#                 "str_len_min": 2,
-#                 "str_len_max": 5,
-#                 "order": "ascending",
-#             },
-#             {
-#                 "generator": "categorical",
-#                 "name": "cat",
-#                 "categories": ["X", "Y", "Z"],
-#                 "probabilities": [0.1, 0.5, 0.4],
-#                 "order": "ascending",
-#             },
-#             {
-#                 "generator": "float",
-#                 "name": "flo",
-#                 "min_value": 3,
-#                 "max_value": 10,
-#                 "sig_figs": 3,
-#                 "order": "ascending",
-#             },
-#         ]
-#         expected_data = [
-#             np.array([21, 23, 30, 36, 57, 60, 62, 70, 70, 87]),
-#             np.array(
-#                 [
-#                     "2003-12-27",
-#                     "2005-11-23",
-#                     "2007-03-10",
-#                     "2008-12-17",
-#                     "2011-04-02",
-#                     "2014-07-16",
-#                     "2015-12-26",
-#                     "2016-02-07",
-#                     "2021-10-01",
-#                     "2021-11-24",
-#                 ]
-#             ),
-#             np.array(
-#                 ["00", "000", "0001", "01", "0100", "10", "10", "100", "1110", "1111"]
-#             ),
-#             np.array(["Y", "Y", "Y", "Y", "Y", "Y", "Z", "Z", "Z", "Z"]),
-#             np.array(
-#                 [3.035, 3.477, 4.234, 4.812, 4.977, 5.131, 5.379, 5.488, 7.318, 7.4]
-#             ),
-#         ]
-#         expected_df = pd.DataFrame.from_dict(
-#             dict(zip(["int", "dat", "txt", "cat", "flo"], expected_data))
-#         )
-#         actual_df = dataset_generator.generate_dataset(
-#             self.rng,
-#             columns_to_generate=columns_to_gen,
-#             dataset_length=self.dataset_length,
-#         )
-#         np.testing.assert_array_equal(actual_df.values, expected_df.values)
 
 #     def test_generate_dataset_with_invalid_generator(self):
 #         columns_to_gen = [{"generator": "non existent generator"}]
